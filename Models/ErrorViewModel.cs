85297a2a6cd63113fd2f8f2a55a95b26541dae83
McKennaGraceTrussel <McKennaGraceTrussel@MCKENNATRUS61D6>
2021-04-12 23:21:17 -0600

    Add project files.

---
+++ Models/ErrorViewModel.cs
@@ -0,0 +1,11 @@
+using System;
+
+namespace MummyTeam.Models
+{
+    public class ErrorViewModel
+    {
+        public string RequestId { get; set; }
+
+        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+    }
+}
